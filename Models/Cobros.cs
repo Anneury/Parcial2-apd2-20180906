ba929e9d08a06d3c9be652d0c621bbe023d531fa
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-06-28 08:27:01 -0400

    Agregando informacion.

--- Models/Cobros.cs
+++ Models/Cobros.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,14 @@ namespace Parcial2_apd2_20180906.Models
 {
     public class Cobros
     {
+        [Key]
+        public int CobroId { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Totales { get; set; }
+        public double TotalCobro { get; set; }
+        public string Observaciones { get; set; }
+
+        [ForeignKey("CobroId")]
+        public virtual List<CobrosDetalle> Detalle { get; set; }
     }
 }
